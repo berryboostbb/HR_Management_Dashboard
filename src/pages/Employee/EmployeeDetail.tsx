e7c441bb1c10d04d81cd32238b6a27c55f2a288b
Amman <ammankashif7@gmail.com>
2026-01-02 13:12:32 +0500

    update

--- src/pages/Employee/EmployeeDetail.tsx
+++ src/pages/Employee/EmployeeDetail.tsx
@@ -1,8 +1,12 @@
 import { FaArrowLeft, FaUserTie } from "react-icons/fa";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function EmployeeDetails() {
   const navigate = useNavigate();
+  const [isEdit, setIsEdit] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [isSaved, setIsSaved] = useState(false);
 
   return (
     <div className="bg-[#F7F7F7] h-[calc(100vh-110px)] rounded-xl p-4 flex flex-col">
@@ -27,7 +31,17 @@ export default function EmployeeDetails() {
               <h3 className="text-[14px] font-semibold">Cristofor Schleifer</h3>
               <p className="text-[#0755E9] text-[12px]">NSM-007</p>
             </div>
-            <button className="h-11 px-4 rounded-md text-white bg-[#0755E9] text-sm">
+            <button
+              onClick={() => setIsEdit(true)}
+              className={`
+    h-11 px-4 rounded-md border text-sm transition cursor-pointer
+    ${
+      isEdit
+        ? "bg-[#0755E9] text-white border-[#0755E9]" // active state
+        : "bg-transparent text-[#0755E9] border-[#0755E9] hover:bg-[#0755E9] hover:text-white"
+    } // default
+  `}
+            >
               Edit
             </button>
           </div>
@@ -103,11 +117,27 @@ export default function EmployeeDetails() {
 
         <div className="mt-5 w-full md:w-1/2">
           <h4 className="text-[16px] mb-3">Password</h4>
-          <div className="relative border border-[#0755E9] rounded-md p-3 text-sm">
-            <span className="absolute left-3 -mt-8 translate-y-1 text-[#7D7D7D] text-sm bg-white border-3 border-white">
-              Password
-            </span>
-            <span className="ml-auto">**********</span>
+
+          <div className="flex items-center gap-3">
+            <div className="relative flex-1 border border-[#0755E9] rounded-md p-3 text-sm">
+              <span className="absolute left-3 -top-2 text-[#7D7D7D] text-xs bg-white px-1">
+                Password
+              </span>
+              <span>**********</span>
+            </div>
+            <button
+              onClick={() => setIsActive(true)}
+              className={`
+        h-11 px-4 rounded-md border text-sm transition cursor-pointer
+        ${
+          isActive
+            ? "bg-[#0755E9] text-white border-[#0755E9]"
+            : "bg-transparent text-[#0755E9] border-[#0755E9] hover:bg-[#0755E9] hover:text-white"
+        }
+      `}
+            >
+              Change Password
+            </button>
           </div>
         </div>
 
@@ -159,6 +189,104 @@ export default function EmployeeDetails() {
               </div>
             </div>
           </div>
+        </div>
+
+        <div className="mt-5">
+          <p className="text-[14px] font-medium mb-2">Leaves</p>
+
+          <div className="border border-[#0755E9] rounded-lg p-4">
+            <div className="grid grid-cols-2 gap-4 text-sm">
+              <div className="flex items-center gap-7">
+                <input
+                  type="checkbox"
+                  defaultChecked
+                  className="accent-[#0755E9]"
+                />
+                <span className="w-24">Annual Leaves</span>
+                <input
+                  type="text"
+                  value="10"
+                  className="w-14 h-7 border rounded text-center"
+                />
+              </div>
+
+              <div className="flex items-center gap-7">
+                <input
+                  type="checkbox"
+                  defaultChecked
+                  className="accent-[#0755E9]"
+                />
+                <span className="w-24">Sick Leaves</span>
+                <input
+                  type="text"
+                  value="05"
+                  className="w-14 h-7 border rounded text-center"
+                />
+              </div>
+
+              <div className="flex items-center gap-7">
+                <input
+                  type="checkbox"
+                  defaultChecked
+                  className="accent-[#0755E9]"
+                />
+                <span className="w-24">Casual Leaves</span>
+                <input
+                  type="text"
+                  value="10"
+                  className="w-14 h-7 border rounded text-center"
+                />
+              </div>
+
+              <div className="flex items-center gap-7">
+                <input
+                  type="checkbox"
+                  checked
+                  disabled
+                  className="accent-[#0755E9]"
+                />
+                <span className="w-24 text-gray-400">Maternity Leaves</span>
+                <input
+                  type="text"
+                  value="0"
+                  disabled
+                  className="w-14 h-7 border rounded text-center bg-gray-100"
+                />
+              </div>
+            </div>
+          </div>
+          {isEdit && (
+            <div className="flex justify-end gap-3 mt-4">
+              <button
+                onClick={() => {
+                  setIsEdit(false);
+                  setIsSaved(false);
+                }}
+                className="
+          px-4 py-2 rounded-md border border-[#0755E9]
+          text-[#0755E9] bg-transparent text-sm
+          hover:bg-[#0755E9] hover:text-white
+          transition cursor-pointer
+        "
+              >
+                Cancel
+              </button>
+
+              <button
+                onClick={() => setIsSaved(true)}
+                className={`
+          px-4 py-2 rounded-md border text-sm transition cursor-pointer
+          ${
+            isSaved
+              ? "bg-[#0755E9] text-white border-[#0755E9]"
+              : "bg-transparent text-[#0755E9] border-[#0755E9] hover:bg-[#0755E9] hover:text-white"
+          } // default
+        `}
+              >
+                Save Changes
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
